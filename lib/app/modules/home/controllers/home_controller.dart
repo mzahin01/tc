c5a19702069dab52de951e592014e9e1c81e40ed
mzahin01 <minhazahin@gmail.com>
2024-10-24 23:39:33 +0600

    why not working?

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,3 +1,15 @@
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  late RxDouble screenWidth;
+  late RxDouble desktopBreakpoint;
+  late RxDouble tabletBreakpoint;
+
+  @override
+  void onInit() {
+    super.onInit();
+    screenWidth = Get.width.obs;
+    desktopBreakpoint = 1200.0.obs;
+    tabletBreakpoint = 800.0.obs;
+  }
+}
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,12 +12,64 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomePage'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'TrainSheba Copy here',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: Center(
+          child: controller.screenWidth.value >
+                  controller.desktopBreakpoint.value
+              ? buildDesktopView()
+              : controller.screenWidth.value > controller.tabletBreakpoint.value
+                  ? buildTabletView()
+                  : buildMobileView()),
     );
+  }
+
+  // Mobile layout
+  Widget buildMobileView() {
+    return Obx(() => Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to TrainSheba (Mobile)',
+              style: TextStyle(fontSize: 16),
+            ),
+            Text(
+              'Screen Width: ${controller.screenWidth.value}',
+              style: const TextStyle(fontSize: 14),
+            ),
+          ],
+        ));
+  }
+
+  // Tablet layout
+  Widget buildTabletView() {
+    return Obx(() => Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to TrainSheba (Tablet)',
+              style: TextStyle(fontSize: 18),
+            ),
+            Text(
+              'Screen Width: ${controller.screenWidth.value}',
+              style: const TextStyle(fontSize: 16),
+            ),
+          ],
+        ));
+  }
+
+  // Desktop layout
+  Widget buildDesktopView() {
+    return Obx(() => Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to TrainSheba (Desktop)',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              'Screen Width: ${controller.screenWidth.value}',
+              style: const TextStyle(fontSize: 18),
+            ),
+          ],
+        ));
   }
 }
