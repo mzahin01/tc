e5909da0a6f4e1406be816321fd34fb6417ac655
mzahin01 <minhazahin@gmail.com>
2024-11-18 15:40:54 +0600

    added icon

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tc/app/shared/const/image_asset.dart';
 import 'package:tc/app/shared/widget/animated/crossfade_wrapper_container.dart';
 import 'package:tc/app/shared/widget/response_view.dart';
 import 'package:tc/app/styles/spacing.dart';
@@ -14,15 +15,17 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Obx(() {
-        return Padding(
-          padding: const EdgeInsets.all(Spacing.xxlarge),
-          child: CrossfadeWrapperContainer(
-            visible: !controller.isLoading.value,
-            loaderHeight: Get.height,
-            child: ResponsiveWidget<HomeController>(
-              pc: buildDesktopView(),
-              tab: buildTabletView(),
-              mobile: buildMobileView(),
+        return SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(Spacing.xxlarge),
+            child: CrossfadeWrapperContainer(
+              visible: !controller.isLoading.value,
+              loaderHeight: Get.height,
+              child: ResponsiveWidget<HomeController>(
+                pc: buildDesktopView(),
+                tab: buildTabletView(),
+                mobile: buildMobileView(),
+              ),
             ),
           ),
         );
@@ -37,6 +40,11 @@ class HomeView extends GetView<HomeController> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Image.asset(
+            ImageAsset.main_icon,
+            width: 200,
+            height: 200,
+          ),
           const Text(
             'Welcome to TrainSheba (Mobile)',
             style: TextStyle(fontSize: 18),
@@ -107,6 +115,11 @@ class HomeView extends GetView<HomeController> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Image.asset(
+            ImageAsset.main_icon,
+            width: 200,
+            height: 200,
+          ),
           const Text(
             'Welcome to TrainSheba (Tablet)',
             style: TextStyle(fontSize: 18),
@@ -187,6 +200,11 @@ class HomeView extends GetView<HomeController> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Image.asset(
+            ImageAsset.main_icon,
+            width: 200,
+            height: 200,
+          ),
           const Text(
             'Welcome to TrainSheba (Desktop)',
             style: TextStyle(fontSize: 18),
