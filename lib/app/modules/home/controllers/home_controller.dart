fded00707cbe780793b0d1f4f4846424beb50e38
mzahin01 <minhazahin@gmail.com>
2024-11-18 11:03:14 +0600

    made a dropdown

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,5 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   RxBool isLoading = false.obs;
+  final List<String> items = [
+    'Item1',
+    'Item2',
+    'Item3',
+    'Item4',
+  ];
+
+  RxnString selectedValue = RxnString(null);
+  TextEditingController textEditingController = TextEditingController();
+
+  // Function to update the selected value
+  void setSelected(String value) {
+    selectedValue.value = value;
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tc/app/shared/widget/animated/crossfade_wrapper_container.dart';
@@ -59,15 +60,91 @@ class HomeView extends GetView<HomeController> {
   // Desktop layout
   Widget buildDesktopView() {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Welcome to TrainSheba (Desktop)',
-            style: TextStyle(fontSize: 20),
-          ),
-        ],
-      ),
+      child: Obx(() {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to TrainSheba (Desktop)',
+              style: TextStyle(fontSize: 20),
+            ),
+            DropdownButtonHideUnderline(
+              child: DropdownButton2<String>(
+                isExpanded: true,
+                hint: Text(
+                  'Select Item',
+                  style: TextStyle(fontSize: 14, color: Colors.black),
+                ),
+                items: controller.items
+                    .map((item) => DropdownMenuItem(
+                          value: item,
+                          child: Text(
+                            item,
+                            style: const TextStyle(
+                              fontSize: 14,
+                            ),
+                          ),
+                        ))
+                    .toList(),
+                value: controller.selectedValue.value,
+                onChanged: (value) {
+                  controller.selectedValue.value = value;
+                },
+                buttonStyleData: const ButtonStyleData(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  height: 40,
+                  width: 200,
+                ),
+                dropdownStyleData: const DropdownStyleData(
+                  maxHeight: 200,
+                ),
+                menuItemStyleData: const MenuItemStyleData(
+                  height: 40,
+                ),
+                dropdownSearchData: DropdownSearchData(
+                  searchController: controller.textEditingController,
+                  searchInnerWidgetHeight: 50,
+                  searchInnerWidget: Container(
+                    height: 50,
+                    padding: const EdgeInsets.only(
+                      top: 8,
+                      bottom: 4,
+                      right: 8,
+                      left: 8,
+                    ),
+                    child: TextFormField(
+                      expands: true,
+                      maxLines: null,
+                      controller: controller.textEditingController,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 8,
+                        ),
+                        hintText: 'Search for an item...',
+                        hintStyle: const TextStyle(fontSize: 12),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                  ),
+                  searchMatchFn: (item, searchValue) {
+                    return item.value.toString().contains(searchValue);
+                  },
+                ),
+                //This to clear the search value when you close the menu
+                onMenuStateChange: (isOpen) {
+                  if (!isOpen) {
+                    controller.textEditingController.clear();
+                  }
+                },
+              ),
+            ),
+          ],
+        );
+      }),
     );
   }
 }
